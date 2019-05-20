66d1f2ffa06e8f06443d4de468c4c4b10e06c34f
Mihail Stoykov <M.Stoikov@gmail.com>
2019-05-20 13:57:35 +0300

    Hide two warnings

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
  var path = require('path');
  var webpack = require('webpack');
  module.exports = {
+     mode: "production",
      entry: './main.js',
      output: {
          path: path.resolve(__dirname, 'build'),
